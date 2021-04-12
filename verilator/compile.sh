fe764aaf5a61696c9c9570a2991bd078843debf2
Moetchadjia Davy Waku Kouomou <mwk3@rg-fpga-dev-1.crnch>
2021-04-11 23:24:22 -0400

    Update makefile and compile script

--- verilator/compile.sh
+++ verilator/compile.sh
@@ -1,11 +1,11 @@
-CHIPYARD_PATH=$SETME
+CHIPYARD_PATH=/tools/reconfig/chisel/chipyard
 
 riscv64-unknown-elf-gcc \
   -I$CHIPYARD_PATH/toolchains/riscv-tools/riscv-tests/env \
   -I$CHIPYARD_PATH/toolchains/riscv-tools/riscv-tests/benchmarks/common \
   -DPREALLOCATE=1 -mcmodel=medany -static -O0 -std=gnu99 -ffast-math -fno-common -fno-builtin-printf \
   -o test.riscv \
-  $CHIPYARD_PATH/sims/verilator/test.c \
+  ../test/PA.c \
   $CHIPYARD_PATH/toolchains/riscv-tools/riscv-tests/build/../benchmarks/common/syscalls.c \
   $CHIPYARD_PATH/toolchains/riscv-tools/riscv-tests/build/../benchmarks/common/crt.S \
   -static -nostdlib -nostartfiles -lm -lgcc -T \
